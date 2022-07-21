15470b680e265c13465fb70a01b8edc5b703ae4c
Adekunle1996 <olaniyandadekunle255@gmail.com>
2022-07-21 08:29:06 +0100

    project-file

--- index.js
+++ index.js
@@ -3,15 +3,21 @@ const bigWrapper=document.querySelector(".big-wrapper");
 const btn=document.querySelector(".toggle");
 let dark=false;
 const animationFtn=function(){
-    const conle=bigWrapper.cloneNode(true);
-    main.appendChild(conle);
     dark=!dark;
+    /*
+    const clone=bigWrapper.cloneNode(true);
+    main.appendChild(clone);
+    */
     if(dark==true){
         bigWrapper.classList.remove("light");
         bigWrapper.classList.add("dark");
+        bigWrapper.classList.add("transition");
+        
     }else{
         bigWrapper.classList.remove("dark");
         bigWrapper.classList.add("light");
+        bigWrapper.classList.add("transition")
+      
     }
 };
 
