ed7c81789bdda78f2a0472289ecdf7781d7ddbb4
Adekunle1996 <olaniyandadekunle255@gmail.com>
2022-07-23 10:02:42 +0100

    project file

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-const main=document.querySelector("main");
+ 
 const bigWrapper=document.querySelector(".big-wrapper");
 const btn=document.querySelector(".toggle");
-let dark=false;
+const main=document.querySelector("main");
+
+
+let dark=false
 const animationFtn=function(){
-    dark=!dark;
-    /*
-    const clone=bigWrapper.cloneNode(true);
-    main.appendChild(clone);
-    */
-    if(dark==true){
+    dark=!dark
+    if(dark== true){
         bigWrapper.classList.remove("light");
         bigWrapper.classList.add("dark");
-        bigWrapper.classList.add("transition");
-        
+        bigWrapper.classList.add("display-effect");
+        bigWrapper.classList.remove("display-re-effect");
+
     }else{
         bigWrapper.classList.remove("dark");
         bigWrapper.classList.add("light");
-        bigWrapper.classList.add("transition")
-      
+        bigWrapper.classList.remove("display-effect");
+        bigWrapper.classList.add("display-re-effect");
+
+    }};
+
+    btn.addEventListener("click", animationFtn);
+
+
+const navMenuSlide=()=>{
+const navMenu=document.querySelector(".nav-menu");
+const burger=document.querySelector(".burger");
+
+burger.addEventListener("click", ()=>{
+    navMenu.classList.toggle("nav-display");
+    const navItem=document.querySelectorAll(".nav-menu li");
+    navItem.forEach((value, index)=>{
+    if(value.style.animation){
+        value.style.animation="";
+    }else{
+        value.style.animation=`nav-item-animate 0.2s ease forwards ${index / 20 + 0.5}s`
     }
-};
+});
+burger.classList.toggle("toggleHam");
+})
+}
 
-
-btn.addEventListener("click", animationFtn);
+navMenuSlide();
